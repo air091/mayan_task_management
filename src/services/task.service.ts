e4685da2263c890cbb8fe3025a253d58c7d47002
air051 <acesparkle243@gmail.com>
2026-06-19 12:53:47 +0800

    get to find by id task

--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -17,7 +17,7 @@ export const createTask = async (taskPayload: ICreateTask): Promise<ITask> => {
   return task;
 };
 
-export const getTaskById = async (id: string) => {
+export const findTaskById = async (id: string) => {
   if (!id) throw new AppError("Task ID is required", 404);
   const task = await prisma.task.findUnique({ where: { id } });
   if (!task) throw new AppError("Task not found", 404);
