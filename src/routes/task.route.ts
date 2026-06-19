0e1cb925bd5676bd6f20601a26f9ac46fb5d7c36
air051 <acesparkle243@gmail.com>
2026-06-19 13:04:10 +0800

    get all tasks route

--- src/routes/task.route.ts
+++ src/routes/task.route.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import { getTaskById, postTask } from "../controllers/task.controller";
+import {
+  getAllTask,
+  getTaskById,
+  postTask,
+} from "../controllers/task.controller";
 
 const router: Router = Router();
 
+router.get("/", getAllTask);
 router.get("/:taskId", getTaskById);
 router.post("/", postTask);
 
