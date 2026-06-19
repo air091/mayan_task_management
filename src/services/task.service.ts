7ed9824a19957fac3a7fc4349277e50297eb2fff
air051 <acesparkle243@gmail.com>
2026-06-19 12:29:31 +0800

    add task service

--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -1,1 +1,18 @@
-const addTask = async (name: string, description: string) => {};
+import { AppError } from "../libs/errors";
+import { prisma } from "../libs/prisma";
+
+export const addTask = async (title: string, description: string) => {
+  if (!title || !description)
+    throw new AppError("All fields are required", 400);
+
+  title = title.trim();
+
+  const task = await prisma.task.create({
+    data: {
+      title,
+      description,
+    },
+  });
+
+  return task;
+};
