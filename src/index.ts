77dfce6bcb9e0c3d5061eeb0a0019f2755daf416
air051 <acesparkle243@gmail.com>
2026-06-19 12:21:19 +0800

    server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
+import "dotenv/config";
 import express from "express";
+
+const app = express();
+const port = process.env.PORT || "8000";
+
+const startServer = () => {
+  try {
+    app.listen(port, () => console.log("Server running in port:", port));
+  } catch (error) {
+    console.error("Start server failed", error);
+    process.exit(1);
+  }
+};
+
+startServer();
