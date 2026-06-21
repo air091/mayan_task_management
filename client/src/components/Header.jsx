f5fae47721cdb2b1655ef072e30b3513c9d45ca0
air051 <acesparkle243@gmail.com>
2026-06-22 00:00:57 +0800

    new task button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,12 @@
-import { Search, X } from "lucide-react";
+import { Plus, Search, X } from "lucide-react";
 import { useTasks } from "../hooks/useTasks";
+import CreateTaskForm from "./CreateTaskForm";
+import { useState } from "react";
 
 const Header = () => {
   const { search, setSearch } = useTasks();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <header>
       <h1 className="flex items-center justify-center gap-x-2 px-4 font-bold text-2xl mb-2">
@@ -10,8 +14,8 @@ const Header = () => {
         <span className="separator border-l border h-8"></span>
         Task Management
       </h1>
-      <div className="flex flex-col items-center justify-center py-2 border">
-        <div className="relative w-full max-w-[360px]">
+      <div className="flex flex-col items-center justify-center w-full max-w-[360px] mx-auto p-2">
+        <div className="relative w-full">
           <input
             type="search"
             placeholder="Search task"
@@ -28,22 +32,34 @@ const Header = () => {
           >
             {search.length > 0 && <X size={20} />}
           </button>
-          <div className="filters border flex items-center justify-end py-1">
-            <button className="px-2 text-[14px] cursor-pointer hover:border-b-2 hover:border-gray-300">
+        </div>
+        <div className="filters w-full flex items-center justify-between py-1">
+          <button
+            onClick={() => setIsModalOpen(true)}
+            className="px-2 flex items-center gap-x-1 bg-green-500 hover:bg-green-600 cursor-pointer rounded-full text-white text-[14px]"
+          >
+            <Plus size={18} /> New Task
+          </button>
+          <div>
+            <button className="px-2 text-[12px] cursor-pointer hover:border-b-2 hover:border-gray-300">
               All
             </button>
-            <button className="px-2 text-[14px] cursor-pointer hover:border-b-2 hover:border-gray-300">
+            <button className="px-2 text-[12px] cursor-pointer hover:border-b-2 hover:border-gray-300">
               Inactive
             </button>
-            <button className="px-2 text-[14px] cursor-pointer hover:border-b-2 hover:border-gray-300">
+            <button className="px-2 text-[12px] cursor-pointer hover:border-b-2 hover:border-gray-300">
               Active
             </button>
-            <button className="px-2 text-[14px] cursor-pointer hover:border-b-2 hover:border-gray-300">
+            <button className="px-2 text-[12px] cursor-pointer hover:border-b-2 hover:border-gray-300">
               Completed
             </button>
           </div>
         </div>
       </div>
+      <CreateTaskForm
+        isModalOpen={isModalOpen}
+        setIsModalOpen={setIsModalOpen}
+      />
     </header>
   );
 };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,25 +8,12 @@ import CreateTaskContent from "../components/CreateTaskForm";
 import CreateTaskForm from "../components/CreateTaskForm";
 
 const Home = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
   return (
     <div className="h-screen py-4">
       <div className="relative mx-auto flex flex-col w-full max-w-[1020px] py-3 px-1 h-full overflow-y-hidden">
         <Header />
         <MainContent />
-        <button
-          onClick={() => setIsModalOpen(true)}
-          className="absolute bottom-2 right-2 block bg-green-500 hover:bg-green-600 cursor-pointer text-white rounded-full p-2"
-        >
-          <Plus size={32} />
-        </button>
       </div>
-
-      <CreateTaskForm
-        isModalOpen={isModalOpen}
-        setIsModalOpen={setIsModalOpen}
-      />
     </div>
   );
 };
